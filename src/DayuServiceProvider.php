3a6e6f698e73082b6f721eacac7965b60648f162
Gabriel Tong <tongyangmath@gmail.com>
2015-12-08 13:58:08 +0800

    0.0.5

--- src/DayuServiceProvider.php
+++ src/DayuServiceProvider.php
@@ -1,25 +1,24 @@
 <?php
 
-namespace Gabe\Dayu\DayuServiceProviders;
+namespace Gabe\Dayu;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\View\Compilers\BladeCompiler;
 
 class DayuServiceProvider extends ServiceProvider
 {
-    public function boot(PermissionRegistrar $permissionLoader)
+    public function boot()
     {
         if (!class_exists('CreateDaYusTable')) {
             $timestamp = date('Y_m_d_His', time());
             $this->publishes([
-                __DIR__.'/../migrations/create_da_yus_table.php.stub' => $this->app->basePath().'/'.'database/migrations/'.$timestamp.'create_da_yus_table.php',
+                __DIR__.'/../migrations/create_da_yus_table.php.stub' => $this->app->basePath().'/'.'database/migrations/'.$timestamp.'_create_da_yus_table.php',
             ], 'migrations');
         }
     }
 
     public function register()
     {
-        
+
 
     }
 
