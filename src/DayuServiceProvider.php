e1d9eb9fac43f7d889995c0b08f8a8d0ee8b02b2
Gabriel Tong <tongyangmath@gmail.com>
2015-12-23 11:43:19 +0800

    0.0.10

--- src/DayuServiceProvider.php
+++ src/DayuServiceProvider.php
@@ -14,13 +14,6 @@ class DayuServiceProvider extends ServiceProvider
                 __DIR__.'/../migrations/create_da_yus_table.php.stub' => $this->app->basePath().'/'.'database/migrations/'.$timestamp.'_create_da_yus_table.php',
             ], 'migrations');
         }
-
-        if (!class_exists('AddSendedAtToGabeDayu')) {
-            $timestamp = date('Y_m_d_His', time());
-            $this->publishes([
-                __DIR__.'/../migrations/add_sended_at_to_gabe_dayu.php.stub' => $this->app->basePath().'/'.'database/migrations/'.$timestamp.'_create_da_yus_table.php',
-            ], 'migrations');
-        }
     }
 
     public function register()
